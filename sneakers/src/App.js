7ed388aafe2068c7a82519529470cbc889ce012f
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-12-27 17:15:49 +0300

    add "drawer" and some styles for it

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -1,9 +1,29 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cartItem d-flex align-center">
+            <img
+              className="mr-20"
+              width={70}
+              height={70}
+              src="/img/sneakers/1.jpg"
+              alt="Sneakers"
+            ></img>
+            <div className="mb-20">
+              <p className="mb-5"> Мужские кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" />
+          <img width={40} height={40} src="/img/logo.png" alt="shop-logo" />
           <div>
             <h3 className="text-uppercase">React Sneakers</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -11,19 +31,30 @@ function App() {
         </div>
         <ul className="d-flex">
           <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" />
+            <img width={18} height={18} src="/img/cart.svg" alt="bag img" />
             <span>1205 руб.</span>
           </li>
           <li>
-            <img width={18} height={18} src="/img/user.svg" />
+            <img width={18} height={18} src="/img/user.svg" alt="user ico" />
           </li>
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center mb-40 justify-between">
+          {" "}
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск...." />
+          </div>
+        </div>
 
         <div className="d-flex">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+
             <img
               width={133}
               height={112}
