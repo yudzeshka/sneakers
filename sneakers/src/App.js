1b8d55525aa3a7c0c5bf30e5e57bd0f506846660
yudzeshka <yudzeshka@gmail.com>
2022-01-03 21:55:42 +0300

    add items search

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
@@ -22,6 +23,9 @@ function App() {
     setCartItems((prev) => [...prev, obj]);
   };
 
+  const onChangeSearshInput = (event) => {
+    setSearchValue(event.target.value);
+  };
   return (
     <div className="wrapper clear">
       {cartOpened && (
@@ -30,24 +34,44 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          {" "}
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу "${searchValue}"`
+              : "Все кроссовки"}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск...." />
+            <input
+              onChange={onChangeSearshInput}
+              value={searchValue}
+              placeholder="Поиск...."
+            />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className="clear cu-p"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            )}
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onPlus={(obj) => onAddToCart(obj)}
-              onFavorite={() => console.log("Добавили в закладки")}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item) => (
+              <Card
+                key={item.title}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onPlus={(obj) => onAddToCart(obj)}
+                onFavorite={() => console.log("Добавили в закладки")}
+              />
+            ))}
         </div>
       </div>
     </div>
